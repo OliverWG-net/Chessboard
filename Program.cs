51b9dc0fdd251b5e65ba8c9e4a39c6476bf7f6c5
Oliver <Oliver@DESKTOP-F7HAQ82>
2024-09-18 10:20:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Chessboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hej vänligen mata hur stort schackbräde du vill ha.");
+            int size;
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0) ;
+            {
+
+            }
+
+        }
+    }
+}
